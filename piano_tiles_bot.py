990418d06f8b6de9fc73859a10f9c037ba5958e3
SHUBHAM THAKUR <shubhamwert@gmail.com>
2020-01-27 01:25:20 +0530

    python piano click bot

---
+++ piano_tiles_bot.py
@@ -0,0 +1,41 @@
+import pyautogui 
+import time
+pyautogui.PAUSE = 0
+pyautogui.FAILSAFE = True
+
+print(pyautogui.size())
+width, height =pyautogui.size()
+
+def getInitialData():
+   x1=0
+   y1=0
+   while True:
+      x, y = pyautogui.position()
+      if x1 != x and y1 !=y:  
+         print(x,y)
+         x1=x
+         y1=y
+
+def click_on_black(x,y):
+   pyautogui.moveTo(x,y)
+   if( pyautogui.pixelMatchesColor(x, y, (0, 0, 0))):
+      pyautogui.click(x=x,y=y)
+   else:
+     return 
+
+
+
+if __name__ == "__main__":
+   # getInitialData()
+   time.sleep(2)
+   x=[714,868,1004,1210]
+
+   #loop initialize
+   while True:
+      for x1 in x:
+         click_on_black(x1,580)
+
+
+
+
+#714 565 868 578 1004 583 1210 589
